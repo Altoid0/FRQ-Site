8997ddad9146f59b6b358cba64cfa7ad71a51cc1
Tanay Shah <tanay.v.shah@gmail.com>
2021-03-24 09:00:31 -0700

    routes

--- src/main/java/com/application/frq/FrqApplicationController.java
+++ src/main/java/com/application/frq/FrqApplicationController.java
@@ -1,4 +1,15 @@
 package com.application.frq;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class FrqApplicationController {
+    @GetMapping("/")
+    public String index(){
+        return "index.html";
+    }
+
+    @GetMapping("/units")
+    public String units(){
+        return "units.html";
+    }
 }
