1e05afa5be7f37b634a11a036278d9ba9e927cff
Tanay Shah <tanay.v.shah@gmail.com>
2021-03-24 08:43:59 -0700

    Initial Commit

---
+++ src/main/java/com/application/frq/FrqApplication.java
@@ -0,0 +1,13 @@
+package com.application.frq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/frq/FrqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.frq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
