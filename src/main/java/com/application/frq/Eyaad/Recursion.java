11f09e32d0b8103d4ad3786ede9f93ef9e4115c4
Eyaad-Emaad <eyaadmir@gmail.com>
2021-03-26 22:04:08 -0700

    First Recursion Commit

---
+++ src/main/java/com/application/frq/Eyaad/Recursion.java
@@ -0,0 +1,52 @@
+package com.application.frq.Eyaad;
+import java.util.Arrays;
+
+public class Recursion {
+    //main function
+    public static void main(String[] args) {
+        Recursion runner = new Recursion();
+        System.out.println(runner.fact(5));
+        System.out.println(runner.gcf(24,9));
+        System.out.println(runner.decimalToBinary(7));
+        System.out.println(runner.digitSum(1357));
+        System.out.println(runner.isPali("racecar"));
+    }
+
+    //Factorial recursion
+    public int fact(int n) { return n>0 ? n*fact(n-1) : 1; }
+
+    //Greatest Common Factor with recursion
+    public int gcf(int a, int b) {
+        if (a % b == 0) {
+            return b;
+        }
+        return gcf(b, a % b);
+    }
+
+    //Convert decimal to binary with recursion
+    public int decimalToBinary(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return n % 2 + 10 * decimalToBinary(n/2);
+    }
+
+    //Adds up all the digits in the input together
+    public int digitSum(int n) {
+        if (n / 10 == 0) {
+            return n;
+        }
+        return n % 10 + digitSum(n / 10);
+    }
+
+    //Checks if string is palindrome or not(doesn't work with spaces/upper case/special characters
+    public boolean isPali(String str) {
+        if (str.length()<=1) {
+            return true;
+        }
+        if (str.charAt(0) == str.charAt(str.length()-1)) {
+            return isPali(str.substring(1,str.length() - 1));
+        }
+        return false;
+    }
+}
