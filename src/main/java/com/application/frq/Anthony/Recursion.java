6a589e656ff363b473de9fdbd599b6ae16be99af
tonyhieu <54915685+tonyhieu@users.noreply.github.com>
2021-03-31 08:47:01 -0700

    set up string methods to return readable results

--- src/main/java/com/application/frq/Anthony/Recursion.java
+++ src/main/java/com/application/frq/Anthony/Recursion.java
@@ -9,9 +9,19 @@ public class Recursion {
         return n>0 ? n*factorial(n-1) : 1;
     }
 
+    public static String returnFac (int n) {
+        int result = factorial(n);
+        return "The factorial of " + n + " is " + result + ".";
+    }
+
     public static int frogsProblem (int steps) { //problem finding the number of ways a frog can hop across a given number of steps if said frog can hop 1 or 2 steps
         if (steps == 1 || steps == 0) 
             return 1;
         return frogsProblem(steps-1) + frogsProblem(steps-2);
+    }
+
+    public static String returnFrog (int steps) {
+        int result = factorial(steps);
+        return "With " + steps + " number of steps, the frog has " + result + " paths that it can go on.";
     }
 }
