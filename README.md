cd85e1532da5941099a520853368041416e59959
Nathaniel Cherian <ngcherian2014@gmail.com>
2021-04-02 13:39:32 -0700

    README

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Simple Spring Boot Site to serve AP CSA FRQs
 
 ## Links
 - [Nathaniel C](http://localhost:8080/nathan/)
+
+## Deployment
+1. Configure maven runner to delegate IDE/Build actions
+2. Build to jar ```target/frq-site.jar```
+3. Move keys to ```keys/kashmir.key``` 
+4. Run ```sh deploy.sh``` 
--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@
 #read password
 #echo okay $password
 
+#read -e -p "jar file to transfer: " jarfile
+#ls -l $jarfile
+
 read -e -p "jar file to transfer: " jarfile
 ls -l $jarfile
 
@@ -15,3 +18,23 @@ ssh -i keys/kashmir.key ubuntu@18.222.9.215 'bash -s' <<- EOM
 EOM
 
 echo "done"
+
+: <<'END'
+
+FILE=target/frq-site.jar
+
+if test -f "$FILE"; then
+  echo "$FILE exists, continuing..."
+  scp -i keys/kashmir.key $jarfile ubuntu@18.222.9.215:/home/ubuntu/frq-site
+  ssh -i keys/kashmir.key ubuntu@18.222.9.215 'bash -s ' <<- EOM
+    cd frq-site/
+    sudo systemctl restart frqsite
+    sudo systemctl restart nginx
+    date >> log
+  EOM
+
+  echo "done"
+else
+  echo "$FILE does not exist, build project first!"
+fi
+END
