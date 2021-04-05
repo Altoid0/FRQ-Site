cedcccdb299c1b0e6521196b4d48c84e4508960a
tonyhieu <54915685+tonyhieu@users.noreply.github.com>
2021-04-04 22:39:14 -0700

    anthony labs

--- src/main/java/com/application/frq/Anthony/Recursion.java
+++ src/main/java/com/application/frq/Anthony/Recursion.java
@@ -1,27 +1,27 @@
 package com.application.frq.Anthony;
 
 public class Recursion {
-    public static void main (String[] args) {
-        System.out.println(factorial(4));
-    }
 
-    public static int factorial (int n) {
+    public int factorial (int n) {
         return n>0 ? n*factorial(n-1) : 1;
     }
 
-    public static String returnFac (int n) {
+    public String returnFac (int n) {
         int result = factorial(n);
         return "The factorial of " + n + " is " + result + ".";
     }
 
-    public static int frogsProblem (int steps) { //problem finding the number of ways a frog can hop across a given number of steps if said frog can hop 1 or 2 steps
+    public int frogsProblem (int steps) { //problem finding the number of ways a frog can hop across a given number of steps if said frog can hop 1 or 2 steps
         if (steps == 1 || steps == 0) 
             return 1;
+        if (steps < 0) {
+            return 0;
+        }
         return frogsProblem(steps-1) + frogsProblem(steps-2);
     }
 
-    public static String returnFrog (int steps) {
-        int result = factorial(steps);
+    public String returnFrog (int steps) {
+        int result = frogsProblem(steps);
         return "With " + steps + " number of steps, the frog has " + result + " paths that it can go on.";
     }
 }
