2e4ed5796203aed9195ef46656fde7abc908324a
DhruvK0 <46884021+DhruvK0@users.noreply.github.com>
2021-04-09 15:24:58 -0700

    fixed public classes issue

--- src/main/java/com/application/frq/Dhruv/Inheritance.java
+++ src/main/java/com/application/frq/Dhruv/Inheritance.java
@@ -6,7 +6,7 @@ import java.sql.Driver;
 import java.util.ArrayList;
 
 public class Inheritance {
-    static class Animal {
+    public static class Animal {
         private String species;
         private String name;
         private int age;
@@ -28,10 +28,10 @@ public class Inheritance {
             return "Species: " + species + ", Name: " + name + ", Age: " + age;
         }
     }
-    static class Bird extends Animal{
+    public static class Bird extends Animal{
         public Bird (String name, int age) {super("Bird", name, age);}
     }
-    static class Giraffe extends Animal{
+    public static class Giraffe extends Animal{
         public Giraffe (String name, int age) {super("Giraffe", name, age);}
     }
 
