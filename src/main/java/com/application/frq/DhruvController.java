5d1aa2d6a9ebaeaeafcd9c8816d3d424661b315d
DhruvK0 <46884021+DhruvK0@users.noreply.github.com>
2021-04-09 15:24:45 -0700

    Added controller logic for inheritance

--- src/main/java/com/application/frq/DhruvController.java
+++ src/main/java/com/application/frq/DhruvController.java
@@ -2,12 +2,15 @@ package com.application.frq;
 
 
 import com.application.frq.Dhruv.Recursion;
+import com.application.frq.Dhruv.Inheritance.*;
 import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 @RequestMapping("/dhruv")
 @Controller
@@ -46,8 +49,19 @@ public class DhruvController {
     }
 
     @GetMapping("/inheritance")
-    public String inheritance() {
-        return "test";
+    public String inheritance(@RequestParam(value = "name", required = false) String name,
+                              @RequestParam(value = "age", required = false) Integer age, Model model) {
+        //Create child class instance bird
+        Bird bird = new Bird(name, age);
+
+
+        //Create child class instance giraffe
+        Giraffe giraffe = new Giraffe(name, age);
+
+        model.addAttribute("birdtext", bird.returnabout());
+        model.addAttribute("girtext", giraffe.returnabout());
+        return "Dhruv/inheritance.html";
     }
+
 
 }
