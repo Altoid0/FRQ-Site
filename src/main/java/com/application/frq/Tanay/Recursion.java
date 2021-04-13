7e5fa9f1cf5a259b8ce3dea92e3cc5d3852eff92
Tanay Shah <tanay.v.shah@gmail.com>
2021-04-12 17:38:36 -0700

    cli user input wow

--- src/main/java/com/application/frq/Tanay/Recursion.java
+++ src/main/java/com/application/frq/Tanay/Recursion.java
@@ -1,5 +1,7 @@
 package com.application.frq.Tanay;
 
+import java.util.Scanner;
+
 public class Recursion {
     public static int factorial(int n) {
         if (n < 2) {
@@ -8,5 +10,11 @@ public class Recursion {
         else {
             return n * factorial(n-1);
         }
+    }
+    public static void main(String[] args){
+        Scanner inputFactorial = new Scanner(System.in);
+        System.out.println("Enter number to take factorial of: ");
+        int userFact = inputFactorial.nextInt();
+        System.out.println(factorial(userFact));
     }
 }
