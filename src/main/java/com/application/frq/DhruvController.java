5b6fbf005b73cf791792654a4bd1b7255e3fad20
DhruvK0 <46884021+DhruvK0@users.noreply.github.com>
2021-04-12 23:33:24 -0700

    Added Insertion logic

--- src/main/java/com/application/frq/DhruvController.java
+++ src/main/java/com/application/frq/DhruvController.java
@@ -1,8 +1,10 @@
 package com.application.frq;
 
 
+import com.application.frq.Dhruv.Insertion;
 import com.application.frq.Dhruv.Recursion;
 import com.application.frq.Dhruv.Inheritance.*;
+import com.application.frq.Dhruv.Insertion.*;
 import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,7 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 @RequestMapping("/dhruv")
 @Controller
@@ -51,6 +55,13 @@ public class DhruvController {
     @GetMapping("/inheritance")
     public String inheritance(@RequestParam(value = "name", required = false) String name,
                               @RequestParam(value = "age", required = false) Integer age, Model model) {
+
+        if (name == null) {
+            name = " ";
+        }
+        if (age == null) {
+            age = 0;
+        }
         //Create child class instance bird
         Bird bird = new Bird(name, age);
 
@@ -62,6 +73,36 @@ public class DhruvController {
         model.addAttribute("girtext", giraffe.returnabout());
         return "Dhruv/inheritance.html";
     }
+
+    @GetMapping("/insertion")
+    public String insertion(@RequestParam(value = "unsortarray", required = false) String unsort, Model model) {
+        if (unsort == null) {
+            unsort = " ";
+        }
+        String str = unsort;
+
+        String[] items = str.replaceAll("\\[", "").replaceAll("\\]", "").replaceAll("\\s", "").split(",");
+
+        int[] results = new int[items.length];
+
+        for (int i = 0; i < items.length; i++) {
+            try {
+                results[i] = Integer.parseInt(items[i]);
+            } catch (NumberFormatException nfe) {
+                //NOTE: write something here if you need to recover from formatting errors
+            };
+        }
+
+        Insertion test = new Insertion();
+        test.Insertion(results);
+
+
+        model.addAttribute("unsort", str);
+        model.addAttribute("sort", Arrays.toString(results));
+
+        return "Dhruv/insertion.html";
+    }
+
 
 
 }
