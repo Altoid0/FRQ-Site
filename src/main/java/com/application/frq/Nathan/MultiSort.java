6a05dfba9bfe01d6b061ec8106f2845edfa99872
Nathaniel Cherian <ngcherian2014@gmail.com>
2021-04-19 12:22:43 -0700

    multisort

--- src/main/java/com/application/frq/Nathan/MultiSort.java
+++ src/main/java/com/application/frq/Nathan/MultiSort.java
@@ -2,6 +2,35 @@ package com.application.frq.Nathan;
 
 import java.util.Arrays;
 
+class Car implements Comparable<Car>{
+    private int price;
+    public Car(int price){
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "price=" + price +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Car c) {
+        if(c.getPrice() < this.getPrice()){
+            return 1;
+        }else if(c.getPrice() > this.getPrice()){
+            return -1;
+        }else{
+            return 0;
+        }
+    }
+}
+
 public class MultiSort<T> {
 
     public <T extends Comparable<T>> T[] insertionSort(T[] arr){
@@ -28,8 +57,16 @@ public class MultiSort<T> {
 
     public static void main(String[] args) {
         String[] strings = new String[]{"nathan", "apple", "classroom", "hat", "cat"};
-        //MultiSort<String> ms = new MultiSort<>();
-        //System.out.println(Arrays.toString(ms.insertionSort(strings)));
+        MultiSort<String> ms = new MultiSort<>();
+        System.out.println(Arrays.toString(ms.insertionSort(strings)));
+
+        Car[] cars = new Car[10];
+        for(int i=0;i<cars.length;i++){
+            cars[i] = new Car((int)(Math.random()*1000));
+        }
+        MultiSort<Car> cs = new MultiSort<>();
+        System.out.println(Arrays.toString(cs.insertionSort(cars)));
+
     }
 
 
