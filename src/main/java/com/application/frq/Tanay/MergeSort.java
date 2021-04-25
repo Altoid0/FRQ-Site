780c8f7dbcb2c3e3223e412cbc5c5aa45332b05b
Tanay Shah <tanay.v.shah@gmail.com>
2021-04-25 12:23:43 -0700

    MergeSort

---
+++ src/main/java/com/application/frq/Tanay/MergeSort.java
@@ -0,0 +1,55 @@
+package com.application.frq.Tanay;
+
+public class MergeSort {
+    public static void mergeSort(int[] a, int n) {
+        if (n < 2) {
+            return;
+        }
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+
+        for (int i = 0; i < mid; i++) {
+            l[i] = a[i];
+        }
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = a[i];
+        }
+        mergeSort(l, mid);
+        mergeSort(r, n - mid);
+
+        merge(a, l, r, mid, n - mid);
+    }
+
+    public static void merge(
+            int[] a, int[] l, int[] r, int left, int right) {
+
+        int i = 0, j = 0, k = 0;
+        while (i < left && j < right) {
+            if (l[i] <= r[j]) {
+                a[k++] = l[i++];
+            }
+            else {
+                a[k++] = r[j++];
+            }
+        }
+        while (i < left) {
+            a[k++] = l[i++];
+        }
+        while (j < right) {
+            a[k++] = r[j++];
+        }
+    }
+
+    public static void main(String[] args){
+        int[] array = {3, 5, 7, 12, 11, 2};
+        mergeSort(array, array.length);
+        System.out.print("Sorted Array: ");
+        for (int i = 0; i < array.length; i++){
+            System.out.print(array[i]);
+            if (i != array.length - 1){
+                System.out.print(", ");
+            }
+        }
+    }
+}
