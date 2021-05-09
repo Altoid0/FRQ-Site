09b1ae0ec59284fd3949bfb0bca61c37475670c2
Nathaniel Cherian <ngcherian2014@gmail.com>
2021-05-08 22:17:25 -0700

    test

---
+++ src/main/java/com/application/frq/Nathan/MoTest.java
@@ -0,0 +1,13 @@
+package com.application.frq.Nathan;
+
+public class MoTest {
+
+    public String test(){
+        return ";";
+    }
+
+    public void test(int g){
+
+    }
+
+}
--- src/main/java/com/application/frq/Nathan/test.java
+++ src/main/java/com/application/frq/Nathan/test.java
@@ -1,6 +1,7 @@
 package com.application.frq.Nathan;
 
 import java.awt.*;
+import java.util.Arrays;
 
 public class test {
 
@@ -10,6 +11,9 @@ public class test {
          String type(){
             return "fi";
         }
+        public Fish(){
+             System.out.println("Fish Contstructed");
+        }
     }
 
     static class Shark extends Fish{
@@ -18,13 +22,39 @@ public class test {
          String type(){
             return "sha";
         }
+
+        public Shark(){
+             int d = 3;
+             System.out.println("Shark Constructed");
+        }
+
+        public Shark(int m){
+
+        }
     }
 
+
+    /**
+     * Changes the array at index 0
+     * @param arr the array reference
+     */
+    static void changeIt(int[] arr){
+        arr[0] = -1;
+        //arr = new int[5];
+    }
+
+
     public static void main(String[] args) {
-        Fish fish = new Fish();
-        int x  = fish.a;
-        Fish shark = new Shark();
-        Shark shark1 = new Shark();
-        System.out.println(fish.type() + " " + shark.type() + " " + shark1.type());
+//        Fish fish = new Fish();
+//        int x  = fish.a;
+            Fish shark = new Shark();
+//        Shark shark1 = new Shark();
+//        System.out.println(fish.type() + " " + shark.type() + " " + shark1.type());
+
+        int[] arr = new int[]{1,2,3,4,5};
+        System.out.println(Arrays.toString(arr));
+        changeIt(arr);
+        System.out.println(Arrays.toString(arr));
+
     }
 }
