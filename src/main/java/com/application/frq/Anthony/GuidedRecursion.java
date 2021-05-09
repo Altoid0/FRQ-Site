10422c80e2e7775a9132d5aff3e143917ac1b9e4
tonyhieu <amv7668@gmail.com>
2021-05-08 17:13:21 -0700

    finished recursion lesson

--- src/main/java/com/application/frq/Anthony/GuidedRecursion.java
+++ src/main/java/com/application/frq/Anthony/GuidedRecursion.java
@@ -3,14 +3,17 @@ package com.application.frq.Anthony;
 import java.util.ArrayList;
 
 public class GuidedRecursion {
-    public ArrayList<String> log = new ArrayList<String>();
+    //keeps track of steps
+    private ArrayList<String> log = new ArrayList<String>();
 
     public int fib (int n) {
+        log.add("<strong>Process for the " + n + "th term</strong>");
+        //edge case for negative numbers
         if (n < 0) {
             log.add("Negative number received, operation ended");
             return -1;
         }
-        log.add("Using switch case to determine what operation to do");
+        //switch statement to check for cases of 0, 1, 2, and everything else
         switch (n) {
             case 0:
                 log.add("Edge case of 0, return 0");
@@ -22,11 +25,13 @@ public class GuidedRecursion {
                 log.add("Case of 2, add 1 to the final result");
                 return 1;
             default:
-                log.add("Value higher than 2 given: adding the last two terms," + (n-2) + "th term of " + fib(n-2) + " and "  + (n-1) + "th term of " + fib(n-1) + " together");
+                log.add("Value higher than 2 given: adding the " + n + "th term and " + (n-1) + "th term together");
+                //recursively calls the function to find the sum of the last two terms
                 return fib(n-1) + fib(n-2);
         }
     }
 
+    //prints steps
     public String dumpLog () {
         String s = "";
         for (int i = 0; i < log.size(); i++) {
